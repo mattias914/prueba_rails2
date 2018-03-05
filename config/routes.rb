389447c047e923a12053a05aa4b7e9b06145c237
matias <mattias914@gmail.com>
2018-03-05 12:18:17 -0300

    intento

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,12 @@ Rails.application.routes.draw do
 
   #   get 'pages#index'
   #  get  'user/new_user' , to 'user#new_user'
-
  #  post 'user_create'  to , 'index/user_create'
+  resources :post
+  root 'post#index'
+  get 'user/sing_up' , to 'user#new_user'
+  post 'user' , to 'user#create_user'
+  resources :sessions, only [:create , :destroy]
+
  # For details on the DSLailable within this file, see http://guides.rubyonrails.org/routing.html
 end
